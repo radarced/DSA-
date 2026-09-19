b36d5b1181d39d337c61b438f4120923783b342a
radar <radar319sucks@gmail.com>
2026-09-19 17:05:29 +0500

    fixed the logic errors
    it can still be improved such as a proper linkedList copy constructor could be implemented and im also traversing duplicates but the code works

--- Knight Travails/knightMoves.js
+++ Knight Travails/knightMoves.js
@@ -10,7 +10,7 @@ function knightMoves(knightPosition,desiredPosition)
         console.log("invalid knigth position");
         return ;
     }
-    if(!inRange(0,7,desiredPosition[1]) && inRange(0,7,desiredPosition[1]))
+    if(!inRange(0,7,desiredPosition[0]) && inRange(0,7,desiredPosition[1]))
     {
         console.log("invalid desired position");
         return ;
@@ -30,22 +30,30 @@ function knightMoves(knightPosition,desiredPosition)
     while(!movesToBeExplored.empty)
     { // while it isnt empty
         let currentExplorableMove = movesToBeExplored.getFirst();
-        let possibleKnightMoves = getKnightMoves(currentExplorableMove.data,lastNode);
-        // console.log(currentExplorableMove.data.join(""),pathsMap);
+        let possibleKnightMoves = getKnightMoves(currentExplorableMove.data);
+
         let oldPath = pathsMap[currentExplorableMove.data.join("")]; // the seperator is necessary for the format to match the pathsMap
         // ^^ this variable points to the linkedList containing the path up untill currentExplorableMove from the original knightPosition.
-        // ^^ axiom : it will always exist.
-        
-        lastNode = currentExplorableMove.data; // this is so that we dont traverse the oldMove itself as a "possible path"
-        // ^^ this variable allows us to guarantee that we never go back in our paths .
-
+        // ^^ axiom : it will always exist.        
+        console.log(possibleKnightMoves.length);
         for(let i = 0;i < possibleKnightMoves.length;i++)
         {
+            let pathName = possibleKnightMoves[i].join("");
+            
             let oldPathArray = oldPath.toValues();
             let newPath = new LinkedList();
             newPath.constructArray(oldPathArray); // construct linkedList from array of nodes ( in order )
             newPath.append(possibleKnightMoves[i]); // add the new move;
-            pathsMap[possibleKnightMoves[i].join("")] = newPath; // append it to our paths container
+            let currentPathLength = newPath.size() - 1;
+
+            if (pathName in pathsMap) {
+                let existingPathLength = pathsMap[pathName].size() - 1;
+                if (currentPathLength > existingPathLength) {
+                    continue; // we dont want to traverse through a path which is slower than an already traversed path.
+                    }
+                }
+            pathsMap[pathName] = newPath; // append it to our paths container
+
             
             if(possibleKnightMoves[i][0] === desiredPosition[0] && possibleKnightMoves[i][1] === desiredPosition[1])
             { // we found the path
@@ -62,6 +70,8 @@ function knightMoves(knightPosition,desiredPosition)
         }
         movesToBeExplored.dequeue(); // remove the processed move;
     }
+    console.log(pathsMap);
+
     if(shortestPaths.length > 1)
     {
     console.log(`You made it in ${shortestLength} moves!  Here's the paths you can take:`);
@@ -74,9 +84,9 @@ function knightMoves(knightPosition,desiredPosition)
         console.log(shortestPaths[i].toString());
     }
 }
-knightMoves([0,0],[3,3]);
+knightMoves([0,0],[1,2]);
 // returns an array of all the possible kngiht positions.
-function getKnightMoves(knightPosition,lastMove)
+function getKnightMoves(knightPosition)
 { // going to assume that the knightPosition is 
     let possibleKnightPositions = [
           [knightPosition[0] - 1,knightPosition[1] + 2],
@@ -93,10 +103,7 @@ function getKnightMoves(knightPosition,lastMove)
     { // 8 possible knight positions
         if(inRange(0,7,possibleKnightPositions[i][0]) && inRange(0,7,possibleKnightPositions[i][1]))
         {
-            if(possibleKnightPositions[i][0] !== lastMove[0] && possibleKnightPositions[i][1] !== lastMove[1])
-            {
             validKnightMoves.push(possibleKnightPositions[i]);
-            }
         }
     }
     return validKnightMoves;
